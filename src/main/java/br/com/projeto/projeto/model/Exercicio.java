862094e8b6bebd42b7c2da8365f68ec08c0ff16a
Matheus gomes <139189013+matheusg0mes@users.noreply.github.com>
2026-04-25 19:24:51 -0300

    adicionado exercicio

--- src/main/java/br/com/projeto/projeto/model/Exercicio.java
+++ src/main/java/br/com/projeto/projeto/model/Exercicio.java
@@ -1,4 +1,64 @@
 package br.com.projeto.projeto.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="exercicio")
 public class Exercicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="nome", nullable = false,length = 45)
+    private String nome;
+
+    @Column(name="descricao", nullable = false,length = 45)
+    private String descricao;
+
+    @Column(name="serie", nullable = false,length = 45)
+    private String serie;
+
+    @Column(name="repeticao", nullable = false,length = 45)
+    private String repeticao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getRepeticao() {
+        return repeticao;
+    }
+
+    public void setRepeticao(String repeticao) {
+        this.repeticao = repeticao;
+    }
 }
